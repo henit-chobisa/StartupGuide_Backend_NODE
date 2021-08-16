7ef0c4114a859a2805119ddcc4ec3b77c0f258e3
Henit Chobisa <henit.chobisa2020@vitstudet.ac.in>
2021-08-17 02:16:10 +0530

    Prefix added

--- app.js
+++ app.js
@@ -7,30 +7,30 @@ const middleWareAuth = require('./middleware/authenticateJWT');
 db();
 app.use(express.json());
 
-app.use('/getBooks', require('./routes/Books/getBooks'));
-app.use('/addBooks', middleWareAuth ,require('./routes/Books/addBooks'));
+app.use('/api/getBooks', require('./routes/Books/getBooks'));
+app.use('/api/addBooks', middleWareAuth ,require('./routes/Books/addBooks'));
 
-app.use('/getBlogs',require('./routes/Blogs/getBlogs'));
-app.use('/addBlogs', middleWareAuth , require('./routes/Blogs/addBlogs'));
+app.use('/api/getBlogs',require('./routes/Blogs/getBlogs'));
+app.use('/api/addBlogs', middleWareAuth , require('./routes/Blogs/addBlogs'));
 
-app.use('/getNewsletters', require('./routes/Newsletters/getNewsletters'));
-app.use('/addNewsletters', middleWareAuth ,require('./routes/Newsletters/addNewsletters'));
+app.use('/api/getNewsletters', require('./routes/Newsletters/getNewsletters'));
+app.use('/api/addNewsletters', middleWareAuth ,require('./routes/Newsletters/addNewsletters'));
 
-app.use('/addStartupStories', middleWareAuth ,require('./routes/StartupStories/addStartupStories'));
-app.use('/getStartupStories', require('./routes/StartupStories/getStartupStories'));
+app.use('/api/addStartupStories', middleWareAuth ,require('./routes/StartupStories/addStartupStories'));
+app.use('/api/getStartupStories', require('./routes/StartupStories/getStartupStories'));
 
-app.use('/getYoutubeChannels', require('./routes/YoutubeChannels/getYoutubeChannels'));
-app.use('addYouTubeChannels',middleWareAuth ,require('./routes/YouTubeChannels/addYoutubeChannel'));
+app.use('/api/getYoutubeChannels', require('./routes/YoutubeChannels/getYoutubeChannels'));
+app.use('/api/addYouTubeChannels',middleWareAuth ,require('./routes/YouTubeChannels/addYoutubeChannel'));
 
-app.use('/getPodcasts', require('./routes/Podcasts/getPodcasts'));
-app.use('/addPodcasts',middleWareAuth , require('./routes/Podcasts/addPodcast'));
+app.use('/api/getPodcasts', require('./routes/Podcasts/getPodcasts'));
+app.use('/api/addPodcasts',middleWareAuth , require('./routes/Podcasts/addPodcast'));
 
-app.use('/getTweets', require('./routes/Tweets/getTweets'));
-app.use('/addTweets', middleWareAuth , require('./routes/Tweets/addTweets'));
+app.use('/api/getTweets', require('./routes/Tweets/getTweets'));
+app.use('/api/addTweets', middleWareAuth , require('./routes/Tweets/addTweets'));
 
-app.use('/signUp',require('./config/register'));
-app.use('/login', require('./config/login'));
-app.use('/getJWT', require('./middleWare/getAToken'));
+app.use('/api/signUp',require('./config/register'));
+app.use('/api/login', require('./config/login'));
+app.use('/api/getJWT', require('./middleWare/getAToken'));
 
 
 app.listen(port, () => {
