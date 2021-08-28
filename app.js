b1480aef2caeaf7f621e20377fa515e1907ade6f
Henit Chobisa <henitchobisa0885@gmail.com>
2021-08-28 21:23:14 +0530

    verificationChanges

--- app.js
+++ app.js
@@ -2,31 +2,48 @@ const express = require('express');
 const db = require('./config/db');
 const app = express();
 const port = process.env.PORT || 5000;
-const middleWareAuth = require('./middleware/verifyWebToken');
+const accessTokenSecret = 'bfwbfwefewjfewhfkweroitj4witji42jtniwitvjwutw094eut0w4u';
 
 db();
 app.use(express.json());
 
+const authenticate = (req,res,next) => {
+    const authHeader = req.headers.authorization;
+    if(authHeader) {
+        const token = authHeader.split(' ')[1];
+        jwt.verify(token, accessTokenSecret, (err, user) => {
+            if (err) {
+                res.send(err);
+            }
+            req.user = user;
+            next();
+        });
+    }
+    else {
+        res.status(404);
+    }
+};
+
 app.use('/api/getBooks' , require('./routes/Books/getBooks'));
-app.use('/api/addBooks', middleWareAuth ,require('./routes/Books/addBooks'));
+app.use('/api/addBooks', authenticate ,require('./routes/Books/addBooks'));
 
 app.use('/api/getBlogs',require('./routes/Blogs/getBlogs'));
-app.use('/api/addBlogs', middleWareAuth , require('./routes/Blogs/addBlogs'));
+app.use('/api/addBlogs', authenticate , require('./routes/Blogs/addBlogs'));
 
 app.use('/api/getNewsletters', require('./routes/Newsletters/getNewsletters'));
-app.use('/api/addNewsletters', middleWareAuth ,require('./routes/Newsletters/addNewsletters'));
+app.use('/api/addNewsletters', authenticate ,require('./routes/Newsletters/addNewsletters'));
 
-app.use('/api/addStartupStories', middleWareAuth ,require('./routes/StartupStories/addStartupStories'));
+app.use('/api/addStartupStories', authenticate ,require('./routes/StartupStories/addStartupStories'));
 app.use('/api/getStartupStories', require('./routes/StartupStories/getStartupStories'));
 
 app.use('/api/getYoutubeChannels', require('./routes/YoutubeChannels/getYoutubeChannels'));
-app.use('/api/addYouTubeChannels',middleWareAuth ,require('./routes/YouTubeChannels/addYoutubeChannel'));
+app.use('/api/addYouTubeChannels',authenticate ,require('./routes/YouTubeChannels/addYoutubeChannel'));
 
 app.use('/api/getPodcasts', require('./routes/Podcasts/getPodcasts'));
-app.use('/api/addPodcasts',middleWareAuth , require('./routes/Podcasts/addPodcast'));
+app.use('/api/addPodcasts',authenticate , require('./routes/Podcasts/addPodcast'));
 
 app.use('/api/getTweets', require('./routes/Tweets/getTweets'));
-app.use('/api/addTweets', middleWareAuth , require('./routes/Tweets/addTweets'));
+app.use('/api/addTweets', authenticate , require('./routes/Tweets/addTweets'));
 
 app.use('/api/auth',require('./config/authentication'));
 
