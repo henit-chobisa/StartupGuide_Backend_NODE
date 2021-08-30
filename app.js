c658c71405b81bf3af66dd62dafe429cd6273fb4
Henit Chobisa <henitchobisa0885@gmail.com>
2021-08-31 00:36:11 +0530

    Deployed

--- app.js
+++ app.js
@@ -11,24 +11,31 @@ app.use(express.json());
 
 app.use('/api/getBooks' , require('./routes/Books/getBooks'));
 app.use('/api/addBooks', authenticate ,require('./routes/Books/addBooks'));
+app.use('/api/getAllBooks', require('./routes/Books/getAllBooks'));
 
 app.use('/api/getBlogs',require('./routes/Blogs/getBlogs'));
 app.use('/api/addBlogs', authenticate , require('./routes/Blogs/addBlogs'));
+app.use('/api/getAllBlogs', require('./routes/Blogs/getAllBlogs'));
 
 app.use('/api/getNewsletters', require('./routes/Newsletters/getNewsletters'));
 app.use('/api/addNewsletters', authenticate ,require('./routes/Newsletters/addNewsletters'));
+app.use('/api/getAllNewsletters', require('./routes/Newsletters/getAllNewsletters'));
 
 app.use('/api/addStartupStories', authenticate ,require('./routes/StartupStories/addStartupStories'));
 app.use('/api/getStartupStories', require('./routes/StartupStories/getStartupStories'));
+app.use('/api/getAllStartupStories', require('./routes/StartupStories/getAllStartupStories'));
 
 app.use('/api/getYoutubeChannels', require('./routes/YoutubeChannels/getYoutubeChannels'));
 app.use('/api/addYouTubeChannels',authenticate ,require('./routes/YoutubeChannels/addYoutubeChannels'));
+app.use('/api/getAllYoutubeChannels', require('./routes/YoutubeChannels/getAllYoutubeChannels'));
 
 app.use('/api/getPodcasts', require('./routes/Podcasts/getPodcasts'));
 app.use('/api/addPodcasts',authenticate , require('./routes/Podcasts/addPodcast'));
+app.use('/api/getAllPodcasts', require('./routes/Podcasts/getAllPodcasts'));
 
 app.use('/api/getTweets', require('./routes/Tweets/getTweets'));
 app.use('/api/addTweets', authenticate , require('./routes/Tweets/addTweets'));
+app.use('/api/getAllTweets', require('./routes/Tweets/getAllTweets'));
 
 app.use('/api/auth',require('./config/authentication'));
 
