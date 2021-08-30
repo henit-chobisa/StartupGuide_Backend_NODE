030c3bab145f68a23e051f1f9c43032cc6e377a9
Henit Chobisa <henitchobisa0885@gmail.com>
2021-08-31 00:00:46 +0530

    fgejnfglew

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const db = require('./config/db');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5500;
 const accessTokenSecret = 'bfwbfwefewjfewhfkweroitj4witji42jtniwitvjwutw094eut0w4u';
 const authenticate = require('./middleWare/authenticate');
 db();
+app.use(cors())
 app.use(express.json());
 
 app.use('/api/getBooks' , require('./routes/Books/getBooks'));
